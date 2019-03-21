fcdbf1edabfe4a919da0a838b0425a5a543e2abf
marko <mark.bencic@gmail.com>
2019-03-21 21:14:31 +0100

    removed links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,14 +84,7 @@
                     <img src="{{ asset('images/under_construction.jpg') }}" alt="working_hard" width="600px" height="500px">
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                
             </div>
         </div>
     </body>
